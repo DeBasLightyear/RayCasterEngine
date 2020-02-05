cfe66ede1bfc938007dcfd0197dac7a6a0c74ccd
BasLightyear <baskleisen@gmail.com>
2020-02-05 16:11:32 +0100

    New color tuple

---
+++ RayCasterEngine/Color.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RayCasterEngine
+{
+    public class Color
+    {
+        public Tuple tuple { get; }
+        public double Red
+        {
+            get { return tuple.Content[0]; }
+        }
+        public double Green
+        {
+            get { return tuple.Content[1]; }
+        }
+        public double Blue
+        {
+            get { return tuple.Content[2]; }
+        }
+
+        public Color(double red, double green, double blue)
+        {
+            tuple = new Tuple(new double[] { red, green, blue });
+        }
+
+        public Color(Tuple t)
+        {
+            tuple = t;
+        }
+    }
+}
